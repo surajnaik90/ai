7ccd6c927e41b658074131427c1e8c2585256e13
Suraj Naik <suraj.b.naik@live.com>
2025-07-12 12:59:05 +0530

    Add compute_cost and compute_gradient functions for linear regression

---
+++ machine-learning/tutorials/compute_cost.py
@@ -0,0 +1,27 @@
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+        x (ndarray): Shape (m,) Input to the model (Population of cities) 
+        y (ndarray): Shape (m,) Label (Actual profits for the cities)
+        w, b (scalar): Parameters of the model
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    # You need to return this variable correctly
+    total_cost = 0
+    
+    ### START CODE HERE ###
+    for i in range(m):
+        total_cost += (w * x[i] + b - y[i]) ** 2
+    
+    total_cost = total_cost / (2 * m)
+    ### END CODE HERE ### 
+
+    return total_cost
