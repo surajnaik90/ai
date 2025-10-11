679aac0b7eb1fbaf8c30b2026e070aa8b5a43b6c
Suraj Naik <suraj.b.naik@live.com>
2025-10-11 19:19:08 +0530

    tensorflow script

---
+++ machine-learning/TensorFlow/Intro-course-labs/tf-1.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=np.float32)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=np.float32)
+
+#Buid a simple sequential model
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(1,)),
+
+    tf.keras.layers.Dense(units=1)
+])
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+model.fit(xs, ys, epochs=500)
+
+print(f"model predict for 10.0: {model.predict([10.0])}")
---
+++ machine-learning/TensorFlow/readme.md
@@ -0,0 +1,8 @@
+# Install TensorFlow
+
+pip install tensorflow
+
+# Resoures to run TensorFlow
+
+https://colab.research.google.com/#
+https://blog.tensorflow.org/2018/07/seedbank-discover-machine-learning.html
