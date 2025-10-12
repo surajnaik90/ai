931101e0e43481af424a66f2fd4d788bb57867e5
Suraj Naik <suraj.b.naik@live.com>
2025-10-12 15:03:47 +0530

    bug fixes

--- machine-learning/TensorFlow/Intro-course-labs/tf-1.py
+++ machine-learning/TensorFlow/Intro-course-labs/tf-1.py
@@ -7,7 +7,6 @@ ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=np.float32)
 #Buid a simple sequential model
 model = tf.keras.Sequential([
     tf.keras.Input(shape=(1,)),
-
     tf.keras.layers.Dense(units=1)
 ])
 
@@ -15,4 +14,6 @@ model.compile(optimizer='sgd', loss='mean_squared_error')
 
 model.fit(xs, ys, epochs=500)
 
-print(f"model predict for 10.0: {model.predict([10.0])}")
+predicted_value = model.predict(np.array([10.0]))
+
+print(f"model predict for 10.0: {predicted_value}")
